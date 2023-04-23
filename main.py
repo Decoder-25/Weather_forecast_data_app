66f902a7f67bb4e547101d03ab8402196d17184c
Disha <bidisha2125@gmail.com>
2023-04-23 13:23:40 +0530

    Filter forecast data and add API key

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
-days = st.slider("Forecast days", min_value=1, max_value=7,
+days = st.slider("Forecast days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+d, t = get_data(place, days, option)
 
-def get_data(days):
-    dates = ["2022-10-25", "2022-10-26", "2022-10-27"]
-    temperature = [10, 12, 15]
-    temperature = [days * i for i in temperature]
-    return dates, temperature
-
-
-d, t = get_data(days)
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
