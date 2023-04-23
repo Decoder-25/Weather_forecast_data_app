4c1f61d445174d18f2571bb8cadb10d564f2b46a
Disha <bidisha2125@gmail.com>
2023-04-23 14:49:07 +0530

    Add images for sky conditions and add try except block

--- main.py
+++ main.py
@@ -10,7 +10,25 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-d, t = get_data(place, days, option)
+if place:
+    try:
+        # Get the temperature and sky data
+        filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        # Create a temperature plot
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures,
+                             labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        # Create the sky image
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write("This is not an existing place.")
